7465f9d2e2ff756425a8e5956cb4184fd8a73476
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-12-29 16:57:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,30 +35,33 @@ ws.onmessage = msg => {
                 account = data["account"]
                 amount = parseInt(data["amount"]);
                 amount = amount * 2
+                subtype = data["subtype"]
                 if (account != "xdg_3r8rzwgq8apcdprr97qfk3sr8mk8r9s4c573g84bg564d96jm7fu44aybjrn") {
                   randomNumber = Math.random() * 2 + 1;
-                  if (randomNumber == 2) {
-                    const postData = {
-                      action: 'send',
-                      wallet: 'B317135FD88904962DC1F9BC206D59FC35CEC5306D32109DDF820C1498172A23',
-                      source: 'xdg_3r8rzwgq8apcdprr97qfk3sr8mk8r9s4c573g84bg564d96jm7fu44aybjrn',
-                      destination: account,
-                      amount: amount
-                    };
-                    const options = {
-                      url: 'http://server.myecoria.com:7030',
-                      method: 'POST',
-                      json: true,
-                      body: postData
-                    };
+                  if (subtype == "send") {
+                    if (randomNumber == 2) {
+                      const postData = {
+                        action: 'send',
+                        wallet: 'B317135FD88904962DC1F9BC206D59FC35CEC5306D32109DDF820C1498172A23',
+                        source: 'xdg_3r8rzwgq8apcdprr97qfk3sr8mk8r9s4c573g84bg564d96jm7fu44aybjrn',
+                        destination: account,
+                        amount: amount
+                      };
+                      const options = {
+                        url: 'http://server.myecoria.com:7030',
+                        method: 'POST',
+                        json: true,
+                        body: postData
+                      };
 
-                    request(options, (error, response, body) => {
-                      if (error) {
-                        console.error(error);
-                      } else {
-                        console.log(response.statusCode, body);
-                      }
-                    });
+                      request(options, (error, response, body) => {
+                        if (error) {
+                          console.error(error);
+                        } else {
+                          console.log(response.statusCode, body);
+                        }
+                      });
+                    }
                   }
                 }
 	}
